b537d52eaac9473f1ffc47bb0893f058cb59fcf9
Steve Hughes <steve.hughes@creative-technologies.co.uk>
2020-05-02 19:47:42 +0100

    Updates

---
+++ precache-manifest.0734f54cf054d71ac2f51daec8c76f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3fd47289fa602ea64582e552b94532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1711d7f874910e2226b",
+    "url": "/static/css/main.dd9cdd01.chunk.css"
+  },
+  {
+    "revision": "4d2955d2dfb24210cbe7",
+    "url": "/static/js/2.ff90239e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff90239e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1711d7f874910e2226b",
+    "url": "/static/js/main.e6699243.chunk.js"
+  },
+  {
+    "revision": "657f30d0a2633971d8bf",
+    "url": "/static/js/runtime-main.7e653150.js"
+  }
+]);
